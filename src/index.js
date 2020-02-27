7c2d6112d3e68d1ae421a57983d1ee65441c7a5a
Pavel Saikov <pavsaikov@gmail.com>
2020-02-27 13:57:38 +0300

    feat: implement functions 'min', 'max', 'avg'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+exports.min = function min(array) {
+    if (arguments.length == 0) return 0;
+    if (array.length == 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
+    let minItem = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minItem) minItem = array[i];
+    }
+
+    return minItem;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (arguments.length == 0) return 0;
+    if (array.length == 0) return 0;
+
+    let maxItem = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > maxItem) maxItem = array[i];
+    }
+
+    return maxItem;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (arguments.length == 0) return 0;
+    if (array.length == 0) return 0;
+
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+
+    let average = sum / array.length;
+
+    return average;
 }
